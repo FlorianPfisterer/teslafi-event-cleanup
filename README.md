adfee613445c45f2ed4a0dd6a08d4ce7000618b3
Florian Pfisterer <florian.pfisterer.2@gmail.com>
2022-01-06 17:59:42 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Drive Data Cleanup
+
+## Usage
+Make sure you copy the original CSV files ('TeslaFi12020.csv' and so on) into the root folder of this repository.
+(For privacy reasons they are not included)
+
+```bash
+source .venv/bin/activate
+python cleanup.py
+```
---
+++ cleanup.py
@@ -0,0 +1,62 @@
+import csv
+
+OUT_FILE_NAME = 'drive-events-merged.csv'
+
+CSV_FILES = [
+    'TeslaFi12020.csv',
+    'TeslaFi12021.csv',
+    'TeslaFi22020.csv',
+    'TeslaFi22021.csv',
+]
+
+SELECTED_PROPERTIES = [
+    'Date',
+
+    'speed',
+    'latitude',
+    'longitude',
+
+    'est_battery_range',
+    'charger_power',
+
+    'outside_temp',
+    'inside_temp',
+    'driver_temp_setting',
+    'passenger_temp_setting',
+    'fan_status',
+    'seat_heater_left',
+    'seat_heater_right',
+    'is_climate_on',
+    'is_auto_conditioning_on'
+]
+
+# read all events
+all_events = []
+for file_name in CSV_FILES:
+    with open(file_name, mode='r') as f:
+        reader = csv.DictReader(f)
+        all_events.extend(reader)
+
+print(f'read {len(all_events)} events from {len(CSV_FILES)} files')
+
+# filter events to include only those that have all selected properties
+def filter_predicate(row):
+    for property in SELECTED_PROPERTIES:
+        if property not in row or row[property] == '':
+            return False
+    return True
+
+filtered_events = list(filter(filter_predicate, all_events))
+print(f'got {len(filtered_events)} events after filtering')
+
+if len(filtered_events) == 0:
+    print('no events found, exiting')
+    exit(0)
+
+# write filtered events to CSV file
+with open(OUT_FILE_NAME, mode='w') as f:
+    writer = csv.DictWriter(f, filtered_events[0].keys())
+    writer.writeheader()
+    writer.writerows(filtered_events)
+
+    print(f'wrote selected events to {OUT_FILE_NAME}')
